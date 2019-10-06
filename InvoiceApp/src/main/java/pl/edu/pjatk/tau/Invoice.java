85332ac4166dc660770e4aea838095052ea97eb0
kamil-wszeborowski <s16215@pjwstk.edu.pl>
2019-10-06 12:12:40 +0200

    Update Invoice.java

--- InvoiceApp/src/main/java/pl/edu/pjatk/tau/Invoice.java
+++ InvoiceApp/src/main/java/pl/edu/pjatk/tau/Invoice.java
@@ -13,67 +13,56 @@ public class Invoice {
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public int getIdKht() {
         return idKht;
     }
-
     public void setIdKht(int idKht) {
         this.idKht = idKht;
     }
-
     public String getInvoiceNumber() {
         return invoiceNumber;
     }
-
     public void setInvoiceNumber(String invoiceNumber) {
         this.invoiceNumber = invoiceNumber;
     }
-
     public float getNetto() {
         return netto;
     }
-
     public void setNetto(float netto) {
         this.netto = netto;
     }
-
     public float getBrutto() {
         return brutto;
     }
-
     public void setBrutto(float brutto) {
         this.brutto = brutto;
     }
-
     public float getVat() {
         return vat;
     }
-
     public void setVat(float vat) {
         this.vat = vat;
     }
-
     public String getVatStr() {
         return vatStr;
     }
-
     public void setVatStr(String vatStr) {
         this.vatStr = vatStr;
     }
-
     public String getDescription() {
         return description;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
 
+
+    private void createInvoice() {
+
+    }
 
 }
 
